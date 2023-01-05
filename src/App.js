7724e75dd42d39b1a1b2adc5cc8b0d7f890cb13c
YuRi7844 <dldbfl7844@gmail.com>
2023-01-05 16:39:34 +0900

    css module 독립성

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className={styles.title}>Welcome back!!!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
 
 export default App;
+/* 
+ 클래스명이 랜덤하게 생성됨. -> 컴포넌트들이나 스타일들이 독립적이다.
+ 굳이 복잡한 클래스명을 외울필요가 없다.
+ 각자의 모듈에서 같은 이름의 클래스를 써도 문제가 없다.
+ */
