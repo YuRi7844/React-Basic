68dabbd4e95dd061620cd2601bafa176aa564faf
YuRi7844 <dldbfl7844@gmail.com>
2023-01-06 14:04:46 +0900

    useEffect를 이용하여 코드 실행조건 제어

--- src/App.js
+++ src/App.js
@@ -4,14 +4,34 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
-  // useEffect : 코드가 딱 1회만 실행되도록 보호한다.
   useEffect(() => {
+    // 코드가 딱 1회만 실행한다.
     console.log("CALL THE API....");
   }, []);
+  useEffect(() => {
+    // keyword가 변화할 때만 코드를 실행한다.
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes.");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
