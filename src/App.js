c3d783a244c46f62fcaeb8586f6b6d3b68d8f352
YuRi7844 <dldbfl7844@gmail.com>
2023-01-06 10:49:36 +0900

    useEffect : 코드가 딱 1회만 실행되도록 보호한다.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  // useEffect : 코드가 딱 1회만 실행되도록 보호한다.
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
 
 export default App;
-/* 
- 클래스명이 랜덤하게 생성됨. -> 컴포넌트들이나 스타일들이 독립적이다.
- 굳이 복잡한 클래스명을 외울필요가 없다.
- 각자의 모듈에서 같은 이름의 클래스를 써도 문제가 없다.
- */
