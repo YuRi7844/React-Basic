18342921b5debe1a4059202418b5dd0752a59d8b
YuRi7844 <dldbfl7844@gmail.com>
2023-01-16 14:51:12 +0900

    fetch 로 api 이용하기, map을 이용하여 arry 사용하기, 응용하여 간단하게 변환기 만들어보기 로딩도 처음써봤음!!

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]); // 여기서 useState 초기값 세팅을 안하면 coins.length가 undefined로 나올수 있겠지
+  const [principal, setPrincipal] = useState(0);
+  const [exc, setExc] = useState("");
+  const onChange = (event) => {
+    setPrincipal(event.target.value);
   };
+  const fnOptionChanged = (event) => {
+    console.log(event.target.value);
+    setExc(principal * event.target.value);
+  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
+      <div>
+        <h1>The Coins! ({coins.length})</h1>
+        {loading ? (
+          <strong>Loading...</strong>
+        ) : (
+          <select onChange={fnOptionChanged}>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+        )}
+      </div>
+      <div>
         <input
           onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
+          value={principal}
+          placeholder="환전할 금액($)을 입력하세요.."
         />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      </div>
+      <h3>환전 예상결과: {exc}</h3>
     </div>
   );
 }
